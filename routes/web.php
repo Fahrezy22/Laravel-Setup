1c40fc6bbdfbdf5068e10033a66a385e00741709
Fahrezy22 <fahriza2222@gmail.com>
2024-08-14 09:08:48 +0700

    setup jwt

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/d', function () {
+        return 'welcome';
+    });
+});
+
+Route::group(['prefix' => 'login'], function(){
+    Route::get('', function () {
+        return view('welcome');
+    })->name('login');
+});
